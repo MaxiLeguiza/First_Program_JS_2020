4cb27285d18806cf9525f4bf4784fc3d92d19693
MaxiLeguiza <leguimanfire17@gmail.com>
2020-04-06 09:19:04 -0300

    Primera practica de js

---
+++ Calculos.js
@@ -0,0 +1,25 @@
+function sumar(a,b){
+    return a + b;
+};
+
+function restar(a,b){
+    return a - b;
+};
+
+function multiplicar(a,b){
+    return a * b;
+};
+
+function dividir(a,b){
+    if(b == 0){
+        console.log('\nNo se puede dividir por 0')
+    }else{
+        return a / b};
+};
+
+exports.sumar=sumar;
+exports.restar=restar;
+exports.multiplicar=multiplicar;
+exports.dividir=dividir;
+
+
---
+++ Principal.js
@@ -0,0 +1,28 @@
+const matematica = require('./Calculos.js');
+
+
+console.log('***********INICIO DE PROGRAMA************');
+console.log('\n');
+console.log('La respuesta de la multiplacion es:')
+console.log('\n');
+console.log(matematica.multiplicar(4,2));
+
+console.log('*****************************************');
+console.log('\n');
+console.log('La respuesta de la suma es:')
+console.log('\n');
+console.log(matematica.sumar(4,2));
+
+console.log('*****************************************');
+console.log('\n');
+console.log('La respuesta de la resta es:')
+console.log('\n');
+console.log(matematica.restar(4,2));
+
+console.log('*****************************************');
+console.log('\n');
+console.log('La respuesta de la divicion es:')
+console.log('\n');
+console.log(matematica.dividir(4,2));
+
+
